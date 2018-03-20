f3b6b3dfbd57796f38858f8a8e143d1622d1dd3d
Fabio Calefato <fcalefato@gmail.com>
2018-03-20 17:50:39 +0100

    update main class

---
+++ src/main/java/it/uniba/main/Main.java
@@ -0,0 +1,13 @@
+package it.uniba.main;
+
+public class Main {
+	/**
+	 * This is the main entry of the application
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
