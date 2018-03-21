bb8c1d948b4d21be064d98fcf7407ae38be5e20c
Fabio Calefato <fcalefato@gmail.com>
2018-03-21 11:57:29 +0100

    add Javadoc comments

--- src/main/java/it/uniba/example/utils/math/MathUtils.java
+++ src/main/java/it/uniba/example/utils/math/MathUtils.java
@@ -4,10 +4,23 @@ package it.uniba.example.utils.math;
  * This class and package are for demo purpose only and must be deleted.
  */
 public class MathUtils {
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public int add(final int a, final int b) {
 		return a + b;
 	}
 	
+	/**
+	 * 
+	 * @param num
+	 * @param div
+	 * @return
+	 * @throws ArithmeticException If </<code>div</code> is zero.
+	 */
 	public float divide(final int num, final int div) throws ArithmeticException {
 		return new Float(num / div);
 	}
