7d6f6f18b984842c32f770f8d118e074b0a0e301
loreloc <lorenzoloconte@outlook.it>
2019-03-26 18:41:26 +0100

    sostituito AppMain.java

--- src/main/java/it/uniba/main/AppMain.java
+++ src/main/java/it/uniba/main/AppMain.java
@@ -1,5 +1,17 @@
 package it.uniba.main;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.GeneralSecurityException;
+import java.util.Map;
+
+import com.google.cloud.bigquery.Job;
+
+import it.uniba.sotorrent.GoogleDocsUtils;
+import it.uniba.sotorrent.ISOQuery;
+import it.uniba.sotorrent.SOQuery;
+
 /**
  * The main class for the project. It must be customized to meet the project
  * assignment specifications.
@@ -16,32 +28,58 @@ public final class AppMain {
 	}
 
 	/**
-	 * This is the main entry of the application.
+	 * 	 * This is the main entry of the application.
 	 *
 	 * @param args
-	 *            The command-line arguments.
+	 *                 The command-line arguments.
+	 * @throws FileNotFoundException See stack trace for proper location.
+	 * @throws IOException  See stack trace for proper location.
+	 * @throws InterruptedException  See stack trace for proper location.
+	 * @throws GeneralSecurityException  See stack trace for proper location.
+	 * @throws URISyntaxException  See stack trace for proper location.
 	 */
-	public static void main(final String[] args) {
-		
-		if(args.length > 0) {
+	public static void main(final String[] args) throws FileNotFoundException,
+												 IOException,
+												 InterruptedException,
+												 GeneralSecurityException,
+												 URISyntaxException {
+		System.out.println("Current working dir: " + System.getProperty("user.dir"));
+
+		if (args.length > 0) {
 			switch (args[0]) {
 			case "it":
-				System.out.println("Ciao mondo");
+				System.out.println(
+						"Seleziona i 10 post piu' 'favoriti' su Stack Overflow "
+						+ "sull'argomento 'google-bigquery'");
 				break;
-				
-			case "es":
-				System.out.println("Hola Mundo");
+
+			case "en":
+				System.out.println("Select the 10 most favorited Stack Overflow posts "
+								   + "on 'google-bigquery'");
 				break;
 
 			default:
-				System.out.println("Specify the language. Languages supported: 'it' or 'es'.");
+				System.out.println("Specify the language. "
+								   + "Languages supported: 'it' or 'en'");
 				break;
-			
+
 			}
 		} else {
-			System.out.println("Hello world.");
+			System.out.println("Using default language 'en'");
+			System.out.println("Select the 10 most favorited Stack Overflow posts "
+							   + "on 'google-bigquery'");
 		}
-		
+
+		ISOQuery soq = new SOQuery();
+		Job job = soq.runQuery();
+		Map<String, Long> res = soq.getResults(job);
+
+		GoogleDocsUtils ut = new GoogleDocsUtils();
+		String spid = ut.createSheet("Prova sna4so");
+		ut.shareSheet(spid);
+		ut.getSheetByTitle(spid);
+		ut.writeSheet(spid, res);
+
 	}
 
 }
