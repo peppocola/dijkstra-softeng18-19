eefbf3b72bbf738cd7442bbc5b3d5e7fb26caab1
peppocola <giuseppecolavito999@gmail.com>
2019-04-20 10:58:19 +0200

    Aggiunto documentazione ArgumentException

--- src/main/java/it/uniba/main/ArgumentException.java
+++ src/main/java/it/uniba/main/ArgumentException.java
@@ -1,7 +1,13 @@
 package it.uniba.main;
-
+/**
+ * Exception thrown when the arguments are invalid.
+ */
 public class ArgumentException extends Exception {
 	
+	/**
+	 * The ArgumentException constructor.
+	 * @param message The error message
+	 */
 	public ArgumentException(String message) {
 		super(message);
 	}
