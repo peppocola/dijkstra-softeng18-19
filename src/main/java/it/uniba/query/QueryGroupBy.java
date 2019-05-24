858b115d99480371927fabe6b6a4d776bd1519ac
loreloc <lorenzoloconte@outlook.it>
2019-05-24 19:28:42 +0200

    aggiunta documentazione alle classi ausiliare del package query

--- src/main/java/it/uniba/query/QueryGroupBy.java
+++ src/main/java/it/uniba/query/QueryGroupBy.java
@@ -1,9 +1,23 @@
 package it.uniba.query;
 
+/**
+ * The SQL syntax 'group by' class. This class generates the 'group by' segment
+ * of the query.
+ * 
+ * <<Entity>>
+ */
 class QueryGroupBy {
 
+	/**
+	 * The attributes strings.
+	 */
 	private String[] attributes;
 
+	/**
+	 * Construct a new QueryGroupBy.
+	 * 
+	 * @param attr The attributes strings.
+	 */
 	QueryGroupBy(final String[] attr) {
 		attributes = attr;
 	}
